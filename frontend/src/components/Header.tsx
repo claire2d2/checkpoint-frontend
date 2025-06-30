36f74a569c50eb53acd304e1b719b34c941aa8e1
Claire <claireyuan.song@gmail.com>
2025-06-30 11:17:45 +0200

    install tw

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,10 +1,10 @@
 import { Link } from "react-router-dom";
 
 export function Header() {
-  return (
-    <header className="header">
-      <h1>Checkpoint : frontend</h1>
-      <Link to="/">Countries</Link>
-    </header>
-  );
+    return (
+        <header className="bg-teal-900">
+            <h1 className="text-white">Checkpoint : frontend</h1>
+            <Link to="/">Countries</Link>
+        </header>
+    );
 }
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,16 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    watch: {
-      usePolling: true,
+    plugins: [react(), tailwindcss()],
+    server: {
+        watch: {
+            usePolling: true,
+        },
+        proxy: {
+            "/api": process.env.API_URL ?? "http://localhost:4000/graphql",
+        },
+        hmr: { path: "hmr" },
     },
-    proxy: {
-      "/api": process.env.API_URL ?? "http://localhost:4000/graphql",
-    },
-    hmr: { path: "hmr" },
-  },
 });
